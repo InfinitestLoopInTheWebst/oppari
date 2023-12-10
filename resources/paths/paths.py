d7f688287fbe0db553ba678ef8f01614f4417bff
InfinitestLoopInTheWebst <63366677+InfinitestLoopInTheWebst@users.noreply.github.com>
2023-12-10 17:56:23 +0200

    remove legacy variables

--- resources/paths/paths.py
+++ resources/paths/paths.py
@@ -13,16 +13,3 @@ class Secrets:
     parent_folder = Path('db_config')
     passfile = Path('passfile')
     pw_database = Path('passwords.kdbx')
-
-
-@dataclass
-class CommentsAndReactions:
-    parent_folder = Path('comments_and_reactions')
-    json_comments = Path('comments.json')
-    json_posts_and_comments = Path('posts_and_comments.json')
-
-
-@dataclass
-class AppsAndWebsitesOffOfFacebook:
-    parent_folder = Path('apps_and_websites_off_of_facebook')
-    json_your_off_facebook_activity = Path('your_off-facebook_activity.json')
